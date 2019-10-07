b6efaf5fb58637b65526a78e85fa23308d587d59
jellyb0y <zuizberg.reg@gmail.com>
2019-10-08 02:49:37 +0300

    проверка

--- web-components/.eslintrc.js
+++ web-components/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "airbnb-base",
+    "extends": "airbnb",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
@@ -14,12 +14,12 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-        "prefer-template": 0,
-        "object-shorthand": 0,
-        "no-restricted-globals": 0,
-        "no-plusplus": 0,
-        "semi": 0,
-        "default-case": 0,
-        "no-case-declarations": 0
+        "prefer-template": 1,
+        "object-shorthand": 1,
+        "no-restricted-globals": 1,
+        "no-plusplus": 1,
+        "semi": 1,
+        "default-case": 1,
+        "no-case-declarations": 1
     }
 };
