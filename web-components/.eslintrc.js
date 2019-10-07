325efd913105f5fec14ba863bcfa74a67043b546
jellyb0y <zuizberg.reg@gmail.com>
2019-10-08 02:56:28 +0300

    проверка

--- web-components/.eslintrc.js
+++ web-components/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
-    "extends": "airbnb",
+    "extends": "airbnb-base",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
